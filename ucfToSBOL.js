123777e8ed6f4a19c226954b165fc1e84dd23279
PrashantVaidyanathan <prash@bu.edu>
2017-05-10 16:35:00 -0400

    fixed component and componentdefinitions

--- ucfToSBOL.js
+++ ucfToSBOL.js
@@ -64,7 +64,7 @@ fs.writeFile("cello.xml", sbol.serializeXML(), function (err) {
 
 function convertPartsToSBOL() {
 
-    Object.keys(partsMap).forEach(function(partkey){
+    Object.keys(partsMap).forEach(function (partkey) {
         var part = partsMap[partkey];
         var partName = part.name;
 
@@ -76,6 +76,8 @@ function convertPartsToSBOL() {
             componentDefinition.persistentIdentity = urlsuffix + componentDefinition.displayId;
             componentDefinition.uri = componentDefinition.persistentIdentity + '/' + componentDefinition.version;
             componentDefinition.wasDerivedFrom = derivedFrom;
+            //add Type
+
 
             const sequence = sbol.sequence()
             sequence.displayId = part.name + '_sequence';
@@ -85,21 +87,24 @@ function convertPartsToSBOL() {
             sequence.persistentIdentity = urlsuffix + sequence.displayId;
             sequence.uri = sequence.persistentIdentity + '/' + sequence.version;
             sequence.wasDerivedFrom = derivedFrom;
+            sequence.encoding = SBOLDocument.terms.dnaSequence;
             componentDefinition.addSequence(sequence)
 
-            componentDefinition.addType(getPartType(part.type));
+            componentDefinition.addRole(getPartType(part.type));
+            componentDefinition.addType(SBOLDocument.terms.dnaRegion);
+
             componentDefinition.addStringAnnotation('http://purl.org/dc/terms/created', datecreated.toISOString() + '');
             var uriVal = componentDefinition.uri;
             partsSBOL[partName] = uriVal;
         }
-    },this);
-    
+    }, this);
+
 
 };
 
-function convertGatePartsToSBOL(){
-    
-    gate_partsArr.forEach(function (gpart){
+function convertGatePartsToSBOL() {
+
+    gate_partsArr.forEach(function (gpart) {
         var gpartName = gpart.gate_name;
         const componentDefinition = sbol.componentDefinition();
         componentDefinition.version = version;
@@ -108,19 +113,29 @@ function convertGatePartsToSBOL(){
         componentDefinition.persistentIdentity = urlsuffix + componentDefinition.displayId;
         componentDefinition.uri = componentDefinition.persistentIdentity + '/' + componentDefinition.version;
         componentDefinition.wasDerivedFrom = derivedFrom;
-        
+
         //Parts in Cassette
-        
-        console.log('Cassettes in ' + gpartName);
-        gpart.expression_cassettes.forEach(function(expression_cassettesArr){
+
+        //console.log('Cassettes in ' + gpartName);
+        gpart.expression_cassettes.forEach(function (expression_cassettesArr) {
             var seq = "";
             var annotationCount = 0;
             var start = 1;
-            
-            expression_cassettesArr.cassette_parts.forEach(function(cassette){
+
+            expression_cassettesArr.cassette_parts.forEach(function (cassette) {
+                
+                const component = sbol.component();
+                component.version = version;
+                component.displayId = cassette;
+                component.name = cassette;
+                component.persistentIdentity = componentDefinition.persistentIdentity + '/' + component.displayId;
+                component.uri = component.persistentIdentity + '/' + component.version;
+                component.definition = sbol.lookupURI(partsSBOL[cassette]);
+                componentDefinition.addComponent(component);
+                
                 var cass_seq = partsMap[cassette].dnasequence;
                 seq += cass_seq;
-                
+
                 const sa = sbol.sequenceAnnotation();
                 sa.displayId = 'annotation' + annotationCount;
                 annotationCount++;
@@ -128,23 +143,20 @@ function convertGatePartsToSBOL(){
                 sa.version = version;
                 sa.persistentIdentity = componentDefinition.persistentIdentity + '/' + sa.displayId;
                 sa.uri = sa.persistentIdentity + '/' + sa.version;
-                sa.addRole(getPartType(partsMap[cassette].type))
+                sa.component = component;
                 sa.description = partsMap[cassette].type;
-                
+
                 const range = sbol.range();
                 range.displayId = 'range';
                 range.persistentIdentity = sa.persistentIdentity + '/' + range.displayId;
                 range.version = version;
                 range.uri = range.persistentIdentity + '/' + range.version;
                 range.start = start;
-                var end = start + cass_seq.length-1;
+                var end = start + cass_seq.length - 1;
                 range.end = end;
-                
+
                 sa.addLocation(range);
-                
                 componentDefinition.addSequenceAnnotation(sa);
-                console.log("For " +  cassette + " URI is " + partsSBOL[cassette]);
-                componentDefinition.addComponent(partsSBOL[cassette].toString());
 
                 start += cass_seq.length;
 
@@ -157,32 +169,32 @@ function convertGatePartsToSBOL(){
             sequence.persistentIdentity = urlsuffix + sequence.displayId;
             sequence.uri = sequence.persistentIdentity + '/' + sequence.version;
             sequence.wasDerivedFrom = derivedFrom;
+            sequence.encoding = SBOLDocument.terms.dnaSequence;
             componentDefinition.addSequence(sequence);
 
-            componentDefinition.addType(gate_parts_so);
-            componentDefinition.addStringAnnotation('http://purl.org/dc/terms/created', datecreated.toISOString() + '');
-            
-            response_funcMap[gpartName].parameters.forEach(function(param){
-                switch(param.name){
-                    case 'ymax':
-                        componentDefinition.addStringAnnotation(ymax, param.value);
-                        break;
-                    case 'ymin':
-                       componentDefinition.addStringAnnotation(ymin, param.value);
-                        break;
-                    case 'K':
-                        componentDefinition.addStringAnnotation(kd, param.value);
-                        break;
-                    case 'n':
-                        componentDefinition.addStringAnnotation(n, param.value);
-                        break;
-                }
-            }, this);
-            componentDefinition.addStringAnnotation(eqn, response_funcMap[gpartName].equation);
-
         }, this);
-        
-
+        /*
+        response_funcMap[gpartName].parameters.forEach(function (param) {
+            switch (param.name) {
+                case 'ymax':
+                    componentDefinition.addStringAnnotation(ymax, param.value);
+                    break;
+                case 'ymin':
+                    componentDefinition.addStringAnnotation(ymin, param.value);
+                    break;
+                case 'K':
+                    componentDefinition.addStringAnnotation(kd, param.value);
+                    break;
+                case 'n':
+                    componentDefinition.addStringAnnotation(n, param.value);
+                    break;
+            }
+        }, this);*/
+        //componentDefinition.addStringAnnotation(eqn, response_funcMap[gpartName].equation);
+    
+        componentDefinition.addType(SBOLDocument.terms.dnaRegion);
+        componentDefinition.addRole(gate_parts_so);
+        componentDefinition.addStringAnnotation('http://purl.org/dc/terms/created', datecreated.toISOString() + '');
     }, this);
 };
 
