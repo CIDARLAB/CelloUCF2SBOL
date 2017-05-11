7985e6647f9fe771c1a1a72884fccc03b55cbf14
PrashantVaidyanathan <prash@bu.edu>
2017-05-11 14:10:29 -0400

    orientation information added

--- ucfToSBOL.js
+++ ucfToSBOL.js
@@ -258,7 +258,8 @@ function convertGatePartsToSBOL() {
                 range.start = start;
                 var end = start + cass_seq.length - 1;
                 range.end = end;
-
+                range.orientation = 'http://sbols.org/v2#inline'
+                
                 sa.addLocation(range);
                 componentDefinition.addSequenceAnnotation(sa);
 
